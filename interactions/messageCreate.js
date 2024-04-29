09067607bab3c34507c5a7422d15c351f59a8935
Carlo <carloflores953@gmail.com>
2024-04-29 17:49:07 -0400

    updated todo list and openai integration

--- interactions/messageCreate.js
+++ interactions/messageCreate.js
@@ -1,11 +1,33 @@
+const axios = require('axios');
 const logger = require('../logger');
-const CLIENT_ID = process.env.CLIENT_ID
+const { OpenAI } = require("openai");
+const CLIENT_ID = process.env.CLIENT_ID;
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
     // Check if the bot has been mentioned
     if (!message.mentions.has(CLIENT_ID)) return;
-      await message.channel.sendTyping()
-      await message.reply("Test message")
+
+    // Start typing to indicate the bot is working on a response
+    await message.channel.sendTyping();
+    const stream = await client.chat.completions.create({
+      model: "gpt-4",
+      messages: [{ role: "user", content: message.content }],
+      stream: true,
+    });
+    sentMessage = await message.channel.send("...");
+    let text = ""
+    let count = 0
+    for await (const chunk of stream) {
+      count += 1
+      text = text.concat(chunk.choices[0]?.delta?.content || "")
+      if (!text || count % 15 != 0) continue;
+      await sentMessage.edit(text);
+    }
+    await sentMessage.edit(text)
   }
 }
+
